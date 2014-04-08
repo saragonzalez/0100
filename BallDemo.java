60e85c3082e433670192fd84b93f45fadf28e959
unknown <sharagoga@gmail.com>
2014-04-08 19:39:20 +0200

    Modificado el codigo para que salga las bolas indicadas por parametro aleatoriamente

--- BallDemo.java
+++ BallDemo.java
@@ -1,5 +1,6 @@
 import java.awt.Color;
-
+import java.util.ArrayList;
+import java.util.Random;
 /**
  * Class BallDemo - a short demonstration showing animation with the 
  * Canvas class. 
@@ -11,19 +12,21 @@ import java.awt.Color;
 public class BallDemo   
 {
     private Canvas myCanvas;
+    private ArrayList <BouncingBall> bolas = new ArrayList<>(); 
 
     /**
      * Create a BallDemo object. Creates a fresh canvas and makes it visible.
      */
     public BallDemo()
     {
+        // 600 es la x y 500 es la y
         myCanvas = new Canvas("Ball Demo", 600, 500);
     }
 
     /**
      * Simulate two bouncing balls
      */
-    public void bounce(int ball)
+    public void bounce(int bola)
     {
         int ground = 400;   // position of the ground line
 
@@ -32,21 +35,34 @@ public class BallDemo
         // draw the ground
         myCanvas.drawLine(50, ground, 550, ground);
 
-        // crate and show the balls
-        BouncingBall ball = new BouncingBall(50, 50, 16, Color.BLUE, ground, myCanvas);
-        ball.draw();
-        BouncingBall ball2 = new BouncingBall(70, 80, 20, Color.RED, ground, myCanvas);
-        ball2.draw();
+        // caja y mostrar las bolas
+        for(int i=0; i<bola; i++){
+
+            Random aleatorio = new Random ();
+            int x = aleatorio.nextInt(300);
+            int y = aleatorio.nextInt(250);
+            int diametro = aleatorio.nextInt(50);
+            int colorRojo = aleatorio.nextInt(256);
+            int colorVerde = aleatorio.nextInt(256);
+            int colorAzul = aleatorio.nextInt(256);
+            
+
+            BouncingBall ball = new BouncingBall(x, y, diametro, new Color(colorRojo, colorVerde, colorAzul), ground, myCanvas);
+            ball.draw();
+            bolas.add(ball);
+        }
 
         // make them bounce
         boolean finished =  false;
         while(!finished) {
-            myCanvas.wait(50);           // small delay
-            ball.move();
-            ball2.move();
+            myCanvas.wait(50);      // small delay
+            for(BouncingBall cadaBola:bolas){
+            cadaBola.move();
+            
             // stop once ball has travelled a certain distance on x axis
-            if(ball.getXPosition() >= 550 || ball2.getXPosition() >= 550) {
+                if(cadaBola.getXPosition() >= 550) {
                 finished = true;
+                }
             }
         }
     }
